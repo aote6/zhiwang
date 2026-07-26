ab9baa63141807ccafcd41790b5be0652cc0cc39
aote6 <aote biu@gmail.com>
2026-07-26 17:16:37 +0800

    07_todo_grep.sh排除_archive/dist等噪音目录

--- collectors/07_todo_grep.sh
+++ collectors/07_todo_grep.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd "$PROJECT_PATH" || exit 0
 echo "### TODO/FIXME ###"
-grep -rn "TODO\|FIXME" --include="*.java" --include="*.py" . 2>/dev/null | grep -vE "build|node_modules"
+grep -rn "TODO\|FIXME" --include="*.java" --include="*.py" . 2>/dev/null \
+  | grep -vE "build|node_modules|_archive|dist|__pycache__|\.smsrepo"
 echo ""
